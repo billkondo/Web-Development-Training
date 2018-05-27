f855fa10ca351fc2cd4d723e44f89b1d26fed9a3
billkondo <willkondo@gmail.com>
2018-05-27 11:18:30 -0300

    - Added Reset Button

--- Free-Code-Camp/Tic-Tac-Toe/main.js
+++ Free-Code-Camp/Tic-Tac-Toe/main.js
@@ -26,33 +26,27 @@ document.getElementById('choiceX').onclick = () => {
 
     let text = document.getElementById('decision-menu').querySelector('#question').innerText;
     let flag = (text[0] === 'P') ? false : true;
-    
-    let currentWindow = document.getElementById('decision-menu');
-    let nextWindow = document.getElementById('game-menu');
 
-    fadeOut(currentWindow, nextWindow, "block");
+    startGame(flag);
     ticTacToeGame(player, flag);
 }
 
-document.getElementById('choiceO').onclick = () => {
-    let player = 0;
-
-    let text = document.getElementById('decision-menu').querySelector('#question').innerText;
-    let flag = (text[0] === 'P') ? false : true;
-    
+const startGame = (flag) => {
     let currentWindow = document.getElementById('decision-menu');
     let nextWindow = document.getElementById('game-menu');
 
     let nx = [nextWindow];
     let types = ["block"];
 
-    /*
-        Prepare the Board
-    */
-
     let scores = document.getElementsByClassName('scoreDisplay');
-    for(let i = 0; i < scores.length; i++) {
-        scores[i].innerHTML = `Player ${i + 1}` + `<br>` + `${0}`;
+
+    for (let i = 0; i < scores.length; i++) {
+        if(!i)
+            scores[i].innerHTML = `Player ${i + 1}` + `<br>` + `${0}`;
+        else {
+            if(!flag) scores[i].innerHTML = `Player ${i + 1}` + `<br>` + `${0}`;
+            else scores[i].innerHTML = `Computer` + `<br>` + `${0}`;
+        }
         nx.push(scores[i]);
         types.push("inline-block");
     }
@@ -66,31 +60,63 @@ document.getElementById('choiceO').onclick = () => {
     resetBoard();
 
     fadeOut(currentWindow, nx, types);
+}
+
+document.getElementById('choiceO').onclick = () => {
+    let player = 0;
+
+    let text = document.getElementById('decision-menu').querySelector('#question').innerText;
+    let flag = (text[0] === 'P') ? false : true;
+
+    startGame(flag);
     ticTacToeGame(player, flag);
+}
+
+document.getElementById('resetAll').onclick = () => {
+    let currentWindow = document.getElementById('game-menu');
+    let nextWindow = document.getElementById('start-menu');
+
+    let scores = document.getElementsByClassName('scoreDisplay');
+    let resetButton = document.getElementById('resetAll');
+
+    objs = [];
+    objs.push(currentWindow);
+    for(let i = 0; i < scores.length; i++) objs.push(scores[i]);
+    objs.push(resetButton);
+
+    for(let i = 0; i < objs.length; i++)
+        if(i != objs.length - 1) 
+            fadeOut(objs[i]);
+        else
+            fadeOut(objs[i], [nextWindow], ["block"]);
 }
 
 const resetBoard = () => {
     let squares = document.getElementsByClassName('square');
 
-    for(let i = 0; i < squares.length; i++) 
+    for (let i = 0; i < squares.length; i++)
         squares[i].innerText = "";
 }
 
 const ticTacToeGame = (player, flag) => {
     let myScore = 0;
-    let otherScore = 0; 
+    let otherScore = 0;
+
+
 }
 
 const fadeOut = (obj, nx, typeDisplay) => {
     obj.style.opacity = 1.0;
-  
+
     const fade = setInterval(() => {
         if (obj.style.opacity < 0.1) {
             obj.style.display = "none";
             clearInterval(fade);
-            
-            for(let i = 0; i < nx.length; i++)
-                fadeIn(nx[i], typeDisplay[i]);
+
+            if(!!nx) {
+                for (let i = 0; i < nx.length; i++)
+                    fadeIn(nx[i], typeDisplay[i]);
+            }
         }
         obj.style.opacity -= 0.2;
     }, 100);
@@ -99,9 +125,6 @@ const fadeOut = (obj, nx, typeDisplay) => {
 const fadeIn = (obj, typeDisplay) => {
     obj.style.opacity = 0.0;
     obj.style.display = typeDisplay;
-
-    console.log(obj);
-    console.log(typeDisplay);
 
     const fade = setInterval(() => {
         if (obj.style.opacity > 0.9) {

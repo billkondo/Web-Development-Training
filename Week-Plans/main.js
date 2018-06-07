ae0895a9c8b4c9e9611d724308cbe7efc129a96e
billkondo <willkondo@gmail.com>
2018-06-06 21:51:55 -0300

    Drag and Drop Events are now possible

--- Week-Plans/main.js
+++ Week-Plans/main.js
@@ -1,4 +1,4 @@
-const startDaysOfWeek = () => {
+const setTitles = () => {
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     let daysOBJ = document.getElementsByClassName('title');
 
@@ -6,35 +6,73 @@ const startDaysOfWeek = () => {
         daysOBJ[i].innerText = days[i];
 }
 
-startDaysOfWeek();
+const setDaysOfWeek = () => {
+    let dayOfWeekObjs = document.getElementsByClassName('dayOfWeek');
+
+    for (let i = 1; i < dayOfWeekObjs.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            let newNode = document.createElement('div');
+            newNode.classList.add('note-empty');
+            dayOfWeekObjs[i].appendChild(newNode);
+        }
+    }
+}
+
+window.onmousedown = (e) => {
+    e.preventDefault();
+}
+
+setTitles();
+setDaysOfWeek();
+
+/* Drag and Drop Functionality */
 
 let objBeingDragged = null;
+let parentOfDragged = null;
 let isMouseDown = false;
 let mouseOffSet = { x: 0, y: 0 };
+let savePosition = { x: 0, y: 0 };
+
+let dropZoneObj = null;
 
 const onMouseDown = (e) => {
     e.preventDefault();
-    console.log('mouseDown');
-    console.log(e.target);
+
     window.addEventListener('mousemove', onMouseMove);
     window.addEventListener('mouseup', onMouseUp);
+
     isMouseDown = true;
     objBeingDragged = e.target;
     objBeingDragged.style.opacity = "0.5";
     mouseOffset = { x: objBeingDragged.offsetLeft - e.clientX, y: objBeingDragged.offsetTop - e.clientY };
+    savePosition = { x: objBeingDragged.style.left, y: objBeingDragged.style.top };
+
+    objBeingDragged.classList.add('no-click');
+
+    parentOfDragged = objBeingDragged.parentElement;
 }
 
 const onMouseUp = (e) => {
-    console.log('mouseUp');
     window.removeEventListener('mousemove', onMouseMove);
     window.removeEventListener('mouseup', onMouseUp);
+
+    objBeingDragged.classList.remove('no-click');
+
+    if (dropZoneObj) {
+        parentOfDragged.removeChild(parentOfDragged.childNodes[0]);
+        dropZoneObj.appendChild(objBeingDragged);
+        resetObj();
+    }
+
     isMouseDown = false;
     objBeingDragged.style.opacity = "1.0";
+    objBeingDragged.style.left = savePosition.x;
+    objBeingDragged.style.top = savePosition.y;
     objBeingDragged = null;
+    parentOfDragged = null;
 }
 
 const onMouseMove = (e) => {
-    console.log('mouseMove');
     e.preventDefault();
     if (isMouseDown) {
         objBeingDragged.style.left = e.clientX + mouseOffset.x + "px";
@@ -42,4 +80,38 @@ const onMouseMove = (e) => {
     }
 }
 
+// Remove this line
 document.getElementById('test').onmousedown = onMouseDown;
+
+const resetObj = () => {
+    if (dropZoneObj) {
+        dropZoneObj.style.border = "none";
+        dropZoneObj.style.backgroundColor = "beige";
+        dropZoneObj = null;
+    }
+}
+
+const startDragEvents = () => {
+    let objNoteEmpty = document.getElementsByClassName('note-empty');
+
+    const onMouseEnter = (e) => {
+        if (!isMouseDown) return;
+        e.preventDefault();
+        dropZoneObj = e.target;
+        dropZoneObj.style.backgroundColor = "lemonchiffon";
+        dropZoneObj.style.border = "3px dashed lightcoral";
+    }
+
+    const onMouseLeave = (e) => {
+        if (!isMouseDown) return;
+        e.preventDefault();
+        resetObj();
+    }
+
+    for (let note of objNoteEmpty) {
+        note.addEventListener('mouseenter', onMouseEnter);
+        note.addEventListener('mouseleave', onMouseLeave);
+    }
+}
+
+startDragEvents();

b91cb34daca0c1aa87dadb200573e3df0d6cc4f3
billkondo <willkondo@gmail.com>
2018-06-12 17:39:24 -0300

    Start Project Configure React

---
+++ Free-Code-Camp/markdown-previewer/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MarkdownPreviewer extends React.Component {
+  render() {
+    return (
+      <div>
+        MarkdownPreviewer
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById('app'));

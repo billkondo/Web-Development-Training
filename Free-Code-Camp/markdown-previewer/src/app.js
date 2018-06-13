e3ca3e7758d33c93353973e9583b298a13179b60
billkondo <willkondo@gmail.com>
2018-06-12 23:55:25 -0300

    Start Content

--- Free-Code-Camp/markdown-previewer/src/app.js
+++ Free-Code-Camp/markdown-previewer/src/app.js
@@ -1,11 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MarkdownPreviewer extends React.Component {
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const startValue = this.props.text;
+
+    return (
+      <div className="Editor">
+        <textarea id="editor"
+          value={startValue}
+          onChange={(e) => this.props.changeMarkdown(e.target.value)}
+        />
+      </div>
+    );
+  }
+}
+
+class Preview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getHTML = this.getHTML.bind(this);
+  }
+
+  getHTML() {
+    return {
+      __html: marked(this.props.text, {
+        sanitize: true
+      })
+    };
+  }
+
   render() {
     return (
-      <div>
-        MarkdownPreviewer
+      <div className="Preview">
+        <div id="prewiew"
+          dangerouslySetInnerHTML={this.getHTML()}
+        />
+      </div>
+    );
+  }
+}
+
+class MarkdownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ""
+    }
+
+    this.changeMarkdown = this.changeMarkdown.bind(this);
+  }
+
+  changeMarkdown(text) {
+    this.setState({ markdown: text });
+  }
+
+  componentWillMount() {
+    const str = "```";
+    const comment = "\\\\";
+
+    const startMarkdown =
+      ` # Welcome to Markdown Previewer
+      \n ## Write markdown in this screen
+      \n To remember the markdown syntax, check this [link](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
+      \n To write inline code, we put the code between back-ticks. Like in this example: \`<div><div>\`.
+      \n ${str}
+      \n ${comment} This is a block code\n let msg = "Hello World!"\n alert(msg)
+      \n ${str}
+      \n Example of lists:
+      \n * Use Asterisk for unordered list\n + You can also use pluses\n - And minuses
+      \n 1. Use Numbers for ordered lists\n 2. Second Item
+      \n\n You can also create blockquotes:
+      `;
+
+    console.log(startMarkdown);
+
+    this.setState({ markdown: startMarkdown })
+  }
+
+  render() {
+    return (
+      <div className="MarkdownPreviewer">
+        <Editor
+          text={this.state.markdown}
+          changeMarkdown={this.changeMarkdown}
+        />
+        <Preview
+          text={this.state.markdown}
+        />
       </div>
     );
   }

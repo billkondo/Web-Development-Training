91051c99fd8e03eb98f9a8ad3e8c575abc8f5b17
billkondo <willkondo@gmail.com>
2018-06-16 02:15:43 -0300

    Header CSS Basic

--- Free-Code-Camp/markdown-previewer/src/app.js
+++ Free-Code-Camp/markdown-previewer/src/app.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.scss'
+
+import './styles.scss';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+      </div>
+    );
+  }
+}
 
 class Editor extends React.Component {
   render() {
@@ -8,6 +18,8 @@ class Editor extends React.Component {
 
     return (
       <div className="Editor">
+        <Header />
+
         <textarea id="editor"
           value={startValue}
           onChange={(e) => this.props.changeMarkdown(e.target.value)}
@@ -27,7 +39,9 @@ class Preview extends React.Component {
   render() {
     return (
       <div className="Preview">
-        <div id="prewiew"
+        <Header />
+
+        <div id="preview"
           dangerouslySetInnerHTML={this.getHTML()}
         />
       </div>
@@ -49,19 +63,19 @@ class MarkdownPreviewer extends React.Component {
     const comment = "\\\\";
 
     const startMarkdown =
-      ` # Welcome to Markdown Previewer
-      \n ## Write markdown in this screen
-      \n To remember the markdown syntax, check this [link](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
-      \n To write inline code, we put the code between back-ticks. Like in this example: \`<div><div>\`.
-      \n ${str}\n ${comment} This is a block code\n\n let msg = "Hello World!"\n alert(msg)\n\n ${str}
-      \n Example of lists:
-      \n * Use Asterisk for unordered lists\n + You can also use pluses\n - And minuses\n
-      \n 1. Use Numbers to create ordered lists\n 2. Second Item from List\n
-      \n You can create blockquotes:
-      \n > Blockquotes are userful to highlight some text\n
-      \n To add an image, you can do this: \n\n![smile](https://upload.wikimedia.org/wikipedia/commons/7/73/Facebook_Haha_React.png)\n
-      \n And finally, this is how we can get text emphasis:
-      \n * Italics: *asterisks* or _underscores_\n * Bold: **asteriks** or __underscores__\n * Mix: **asterisks and _underscores_**\n * Strikethrough: ~~two tildes~~
+      `# Welcome to Markdown Previewer
+      \n## Write markdown in this screen
+      \nTo remember the markdown syntax, check this [link](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
+      \nTo write inline code, we put the code between back-ticks. Like in this example: \`<div><div>\`.
+      \n${str}\n${comment} This is a block code\n\nlet msg = "Hello World!"\nalert(msg)\n\n${str}
+      \nExample of lists:
+      \n* Use Asterisk for unordered lists\n+ You can also use pluses\n- And minuses\n
+      \n1. Use Numbers to create ordered lists\n2. Second Item from List\n
+      \nYou can create blockquotes:
+      \n> Blockquotes are userful to highlight some text\n
+      \nTo add an image, you can do this: \n\n![smile](https://upload.wikimedia.org/wikipedia/commons/7/73/Facebook_Haha_React.png)\n
+      \nAnd finally, this is how we can get text emphasis:
+      \n* Italics: *asterisks* or _underscores_\n* Bold: **asteriks** or __underscores__\n* Mix: **asterisks and _underscores_**\n* Strikethrough: ~~two tildes~~
       `;
 
     this.setState({ markdown: startMarkdown })

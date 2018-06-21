e4f63c60f1a9d5b91d857757d3de64041e8a9439
billkondo <willkondo@gmail.com>
2018-06-21 15:19:32 -0300

    Button Light

--- Free-Code-Camp/drum-machine/src/app.js
+++ Free-Code-Camp/drum-machine/src/app.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
 const idArray = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
 const keyCodeArray = [81, 87, 69, 65, 83, 68, 90, 88, 67];
+
 const nameArray = [
   'Heater 1.0',
   'Heater 2.0',
@@ -15,6 +17,7 @@ const nameArray = [
   'Chord 3.0',
   'Guitar Chord'
 ];
+
 const urlArray = [
   'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
   'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
@@ -27,23 +30,45 @@ const urlArray = [
   'http://scruss.com/wordpress/wp-content/uploads/2017/12/chord-G.wav'
 ];
 
+const ColorsArray = [
+  "#FD3A4A", // Red Salsa
+  "#FA5B3D", // Orange Soda
+  "#FFF700", // Yellow Sunshine
+  "#A7F432", // Green Lizard
+  "#2243B6", // Denim Blue
+  "#9C51B6", // Purple Prim
+  "#FF5470", // Fiery Rose
+  "#FF007C", // Winter Sky
+  "#E936A7"  // Frostbite
+];
+
 const Display = (props) => {
+  const styles = {
+    borderColor: `${props.borderColor}`
+  }
+
   return (
-    <div id="display">
+    <div id="display"
+      style={styles}
+    >
       {props.toDisplay}
     </div>
   );
 }
 
 const Button = (props) => {
-  const play = (e) => {
-    e.target.play();
+  const styles = {
+    borderColor: `${props.color}`,
+    color: `${props.color}`
   }
+
   return (
     <div
       className="drum-pad"
       id={props.id}
-      onClick={(e) => props.click(props.name, props.index)}
+      onMouseDown={(e) => props.mousedown(props.name, props.index)}
+      onMouseUp={(e) => props.mouseup(props.index)}
+      style={styles}
     >
 
       <audio
@@ -69,9 +94,11 @@ const Buttons = (props) => {
               key={curElement}
               id={idArray[index]}
               name={nameArray[index]}
-              click={props.click}
+              mousedown={props.mousedown}
+              mouseup={props.mouseup}
               source={urlArray[index]}
               index={index}
+              color={props.on ? ColorsArray[index] : "#1B1B1B"}
             />
           )
         })
@@ -106,19 +133,17 @@ const Options = (props) => {
 }
 
 const Power = (props) => {
-  const styleON = {
-
-  }
-
-  const stuleOFF = {
-
-  }
-
   return (
-    <div id="power" onClick={props.flip}>
-      {props.on ? "OFF" : "ON"}
+    <div id="power" onClick={props.flip}
+      className={props.on ? "powerIsOn" : "powerIsOff"}
+    >
+      {!props.on ? "OFF" : "ON"}
     </div>
   );
+}
+
+const Control = (props) => {
+  
 }
 
 class DrumMachine extends React.Component {
@@ -126,7 +151,8 @@ class DrumMachine extends React.Component {
     on: false,
     nameToDisplay: "",
     volume: 50,
-    option: false
+    option: false,
+    borderColor: "black"
   }
 
   playSound = (index) => {
@@ -136,14 +162,21 @@ class DrumMachine extends React.Component {
     audio.play();
   }
 
-  clickButton = (soundName, index) => {
+  mouseDown = (soundName, index) => {
     if(this.state.on) {
       this.setState({
         nameToDisplay: soundName,
+        borderColor: ColorsArray[index]
       });
 
       this.playSound(index);
+      document.getElementById(idArray[index]).classList.toggle('buttonPressed');
     }
+  }
+
+  mouseUp = (index) => {
+    if(this.state.on) 
+      document.getElementById(idArray[index]).classList.toggle('buttonPressed');
   }
 
   keyDown = (e) => {
@@ -152,10 +185,20 @@ class DrumMachine extends React.Component {
         if (keyCodeArray[i] === e.keyCode) {
           this.setState({
             nameToDisplay: nameArray[i],
+            borderColor: ColorsArray[i]
           });
 
           this.playSound(i);
+          document.getElementById(idArray[i]).classList.toggle('buttonPressed');
         }
+    }
+  }
+
+  keyUp = (e) => {
+    if (this.state.on) {
+      for (let i = 0; i < keyCodeArray.length; i++)
+        if (keyCodeArray[i] === e.keyCode) 
+          document.getElementById(idArray[i]).classList.toggle('buttonPressed');
     }
   }
 
@@ -163,7 +206,8 @@ class DrumMachine extends React.Component {
     this.setState((prevState) => {
       return { 
         on: !prevState.on,
-        nameToDisplay: ""
+        nameToDisplay: "",
+        borderColor: "black"
       };
     });
   }
@@ -183,10 +227,12 @@ class DrumMachine extends React.Component {
 
   componentDidMount() {
     window.addEventListener('keydown', this.keyDown);
+    window.addEventListener('keyup', this.keyUp);
   }
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.keyDown);
+    window.addEventListener('keyup', this.keyUp);
   }
 
   render() {
@@ -200,10 +246,13 @@ class DrumMachine extends React.Component {
         <Display
           on={this.state.on}
           toDisplay={this.state.nameToDisplay}
+          borderColor={this.state.borderColor}
         />
 
         <Buttons
-          click={this.clickButton}
+          mousedown={this.mouseDown}
+          mouseup={this.mouseUp}
+          on={this.state.on}
         />
 
         <Volume

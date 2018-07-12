365f856cca74a22ac51cca947dfe26add28da2d7
billkondo <willkondo@gmail.com>
2018-07-12 00:32:51 -0300

    Styles Update

--- Free-Code-Camp/pomodoro-clock/src/app.js
+++ Free-Code-Camp/pomodoro-clock/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { CreateStore } from 'redux';
+import './styles.scss';
 
 const Label = (props) => (
   <div id={props.id}>
@@ -12,31 +13,49 @@ const Label = (props) => (
 const BreakSettings = () => (
   <div id="break-settings">
     <Label id="break-label" label="Break Length" />
-    <div id="break-decrement"><i className="fas fa-arrow-down" /></div>
-    <div id="break-length">oi</div> 
-    <div id="break-increment"><i className="fas fa-arrow-up" /></div>
+    <div id="control-settings">
+      <div id="break-decrement"><i className="fas fa-arrow-down" /></div>
+      <div id="break-length">oi</div>
+      <div id="break-increment"><i className="fas fa-arrow-up" /></div>
+    </div>
   </div>
 );
 
 const SessionSettings = () => (
   <div id="session-settings">
     <Label id="session-label" label="Session Length" />
-    <div id="session-decrement"><i className="fas fa-arrow-down" /></div>
-    <div id="session-length">oi</div> 
-    <div id="session-increment"><i className="fas fa-arrow-up" /></div>
+    <div id="control-settings">
+      <div id="session-decrement"><i className="fas fa-arrow-down" /></div>
+      <div id="session-length">oi</div>
+      <div id="session-increment"><i className="fas fa-arrow-up" /></div>
+    </div>
   </div>
 );
 
-const Control = () => (
-  <div id="start-stop">
-    <i className="fas fa-play" />
-    <i className="fas fa-pause" />
+const Settings = () => (
+  <div id="settings">
+    <BreakSettings />
+    <SessionSettings />
+  </div>
+);
+
+const Control = ({ type }) => (
+  <div id="control">
+    <div id="start-stop">
+      {!type && <i className="fas fa-play" />}
+      {type && <i className="fas fa-pause" />}
+    </div>
+
+    <div id="reset">
+      <i className="fas fa-sync-alt" />
+    </div>
   </div>
 );
 
 const Timer = () => (
   <div id="timer">
-    <Label id="timer-label" label="oi" />
+    <Label id="timer-label" label="Session" />
+    <div id="time-left">50</div>
     <Control />
   </div>
 );
@@ -44,8 +63,7 @@ const Timer = () => (
 
 const PomodoroClock = () => (
   <div id="pomodoro-clock">
-    <BreakSettings />
-    <SessionSettings />
+    <Settings />
     <Timer />
   </div>
 );
